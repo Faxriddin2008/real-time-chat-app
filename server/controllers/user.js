91ed1d23f07d75071e32521fc6b20d8bd21a683c
Faxriddin2008 <fabduvasiyev@gmail.com>
2023-09-25 22:44:06 +0500

    feat: register user complete (backend)

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,18 +3,35 @@ const bcrypt = require("bcrypt");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const Users = require("../models/user");
-exports.registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
-  let user = await Users.findOne({ email });
-  if (user) return res.status(400).json("User already exist with this email");
-  if (!validator.isEmail(email))
-    return res.status(400).json("Email is not valid");
-  if (!validator.isStrongPassword(password))
-    return res.status(400).json("Password is not strong enough");
 
-  user = await Users.create({
-    name,
-    email,
-    password: await bcrypt.hash(password, 10),
-  });
+const createToken = (_id) => {
+  const jwt_key = process.env.JWT_SECRET_KEY;
+
+  return jwt.sign({ _id }, jwt_key, { expiresIn: "30d" });
+};
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    let user = await Users.findOne({ email });
+    if (user) return res.status(400).json("User already exist with this email");
+    if (!validator.isEmail(email))
+      return res.status(400).json("Email is not valid");
+    if (!validator.isStrongPassword(password))
+      return res.status(400).json("Password is not strong enough");
+
+    user = await Users.create({
+      name,
+      email,
+      password: await bcrypt.hash(password, 10),
+    });
+
+    await user.save();
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ _id: user._id, name, email, token });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { registerUser } = require("../controllers/user");
 const router = express.Router();
-router.get("/register", registerUser);
+router.post("/register", registerUser);
 
 module.exports = router;
