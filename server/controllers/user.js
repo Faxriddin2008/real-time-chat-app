008fc16bbf7e46cccfac92031443673500f7e7f0
Faxriddin2008 <fabduvasiyev@gmail.com>
2023-09-26 17:01:27 +0500

    feat: add get all users

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -63,3 +63,12 @@ exports.findUser = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await Users.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const { registerUser, loginUser, findUser } = require("../controllers/user");
+const {
+  registerUser,
+  loginUser,
+  findUser,
+  getUsers,
+} = require("../controllers/user");
 const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/find/:userId", findUser);
+router.get("/", getUsers);
+
 module.exports = router;
