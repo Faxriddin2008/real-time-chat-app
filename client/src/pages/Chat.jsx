04bc92377f25791c5147d91802abed8ecfbc26f0
Faxriddin2008 <fabduvasiyev@gmail.com>
2023-09-28 15:29:24 +0500

    feat: add custom styles for chats

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,12 +1,25 @@
 import { useContext } from "react";
 import { ChatContext } from "../context/ChatContext";
+import { Container, Stack } from "react-bootstrap";
 
 const Chat = () => {
   const { userChats, isUserChatsLoading, userChatsError } =
     useContext(ChatContext);
   return (
     <>
-      <p>Chat</p>
+      <Container>
+        {userChats?.length < 1 ? null : (
+          <Stack direction="horizontal" gap={4} className="align-items-start">
+            <Stack className="flex-grow-0 messages-box pe-3" gap={3}>
+              {isUserChatsLoading && <p>Loading chats...</p>}
+              {userChats?.map((chat, index) => {
+                return <div key={index}></div>;
+              })}
+            </Stack>
+            <p>ChatBox</p>
+          </Stack>
+        )}
+      </Container>
     </>
   );
 };
